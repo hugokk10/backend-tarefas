e337c414e8a6e966399d2abde3735691ea37bd22
hugokk10 <hugo.rosario@escola.pr.gov.br>
2026-03-02 10:53:16 -0300

    Add files via upload

---
+++ idade.js
@@ -0,0 +1,6 @@
+let idade =18;
+let dataNascimento = 2008;
+
+console.log("minha idade é:" + idade);
+console.log("meu ano de nascimento é:" + dataNascimento);
+console.log(`minha idade é: ${idade} e meu ano de nascimento é: ${dataNascimento}`);
