28dc9ed83b45a484a53eb68cb23e37cef4835b2d
hugokk10 <hugo.rosario@escola.pr.gov.br>
2026-03-10 08:25:31 -0300

    Add files via upload

---
+++ relatorio.js
@@ -0,0 +1,12 @@
+//relatorio de notas 
+console,log("Relatorio de notas");
+
+let aluno = "hugo"
+let nota1 = 8;
+let nota2 = 7;
+let nota3 = 4
+let media = (nota + nota + nota3) / 3;
+
+//let relatorio = "Aluno: "aluno +"\n"+n" + "notas: " + nota1 + " + nota1 + ", "+ nota2 + "," + noto3;
+let relatorio = "aluno: " + aluno + "| notas: " + nota1 + "e" + nota2 + "| media: "  media;
+console.log(relatorio);
