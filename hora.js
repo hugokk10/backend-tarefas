b496c2aec6bf9ef1edf2f9ada7c846d2ce9647d1
hugokk10 <hugo.rosario@escola.pr.gov.br>
2026-03-16 13:52:16 +0000

    versao1

--- hora.js
+++ hora.js
@@ -1,1 +1,18 @@
 //valores iniciais do dia
+let valorPorhora =50;
+let horasTrabalhadas -8; // expediente padrao
+
+console.log("---Resumo do expediente ---");
+console.log('horas iniciais: +${horasTrabalhadas'h);
+
+HoresTrabalhadas++;
+console.log('apos hora extra (horastrabalhadas++'): ${HorasTrabalhadas}h');
+
+//no dia seguinte ele precisou sair uma hora mais cedo para ir ao medico
+horastrabalhadas++;
+console.log(`apos saida antecipada (horastrabalhadas--): ${horastralhadas}h');
+
+    //calculo final
+    let texasistema = 15;
+    let pagamentos = (horastrabalhadas + valorporhora) - taxasistema;
+    
