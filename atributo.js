9d45ba5ae62dc1ad7aa74c5f67beccf4fae8d2c0
hugokk10 <hugo.rosario@escola.pr.gov.br>
2026-04-28 11:47:36 +0000

    versao1

---
+++ atributo.js
@@ -0,0 +1,17 @@
+let numero = 2;
+numero = numero * 2;
+
+console.log(número);
+
+let numeroN = 2;
+númeroN -= 2;
+console.log(númeroN);
+
+const idade Dandara = 26;
+const minhaIdade = 16;
+
+const saoIguais = idadeDandara == minhaIdade;
+console.log(`As idades são iguais?` + saoIguais);
+
+const comparacao = idadeDandara > minhaIdade;
+console.log(``)
