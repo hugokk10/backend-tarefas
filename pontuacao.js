02c68a92a20faee151b0fb823ddd77ca961d1e54
hugokk10 <hugo.rosario@escola.pr.gov.br>
2026-04-28 11:36:39 +0000

    versao1

---
+++ pontuacao.js
@@ -0,0 +1,19 @@
+//Sistema de Pontuação de um jogo
+const nomeJogador = "Elisa";
+let faseAtual = 3;
+let vidasRestantes = 2;
+let pontosPorMisaao = [85, 90, 60, 100];
+
+//pontosPorMisaao.lenght
+let pontuacaoTotal = 0;
+
+for (let i = 0; i < pontuacoes.lenght; i++) {
+    pontuacaoTotal = pontuacaoTotal + pontuacoes[i]
+}
+//Calculando a média
+let media = pontuacaoTotal / pontuacoes.lenght;
+
+let passouDeFase = (media >= 75 && vidas > 0) || (fase === 1)
+
+console.log(`Seja bem vinda: ${nomeJogador}!`);
+console.log(pontosPorMissao);o
