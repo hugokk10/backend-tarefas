cd41a0ff617c0b87a2c861a9a9a2879e1aca4518
hugokk10 <hugo.rosario@escola.pr.gov.br>
2026-04-28 11:33:42 +0000

    versao1

---
+++ objeto.js
@@ -0,0 +1,23 @@
+const bancoAutomotivo = {
+    material: "Couro Natural",
+    cor: "Caramelo",
+    tamanho: "Padrão",
+    concluído: false
+}
+
+//console.log("Material escolhido: ", bancoAutomotivo.material);
+
+//bancoAutomotivo.concluido = true;
+//console.log(bancoAutomotivo);
+// Criando o objeto produto
+
+const produto = {
+    nome: "iPhone 15 Pro",
+    marca: "Apple",
+    preço: 8999.90,
+    estoque: 15,
+    emPromocao: true
+};
+
+// Exibindo o objeto completo no console
+console.log(produto);
